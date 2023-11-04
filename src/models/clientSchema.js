bdde8ca73b06fa63b98ae165af0962d2cc6563e8
Patricia Eihara <pateihara@gmail.com>
2023-11-04 04:33:06 -0300

    ataulizacao

--- src/models/clientSchema.js
+++ src/models/clientSchema.js
@@ -8,7 +8,14 @@ const clientSchema = new mongoose.Schema({
   },
   cpf: {
     type: String,
-    required: true,
+    required: [true, "CPF is required"], // Mensagem de erro personalizada
+    validate: {
+      validator: function (value) {
+        // Função de validação personalizada
+        return value.trim().length > 0; // Verifica se a string não está vazia
+      },
+      message: "CPF cannot be an empty string", // Mensagem de erro personalizada para a validação
+    },
   },
   contacts: [
     {
