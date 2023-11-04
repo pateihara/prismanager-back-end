efb23c024c1add1552fb98abeb65a636b9429d71
Patricia Eihara <pateihara@gmail.com>
2023-11-04 01:40:26 -0300

    ataulizacao

--- src/models/clientSchema.js
+++ src/models/clientSchema.js
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 
 const clientSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+  },
+  CPF: {
+    type: String,
+    required: true,
+  },
   contacts: [
     {
       type: mongoose.Schema.Types.ObjectId,
