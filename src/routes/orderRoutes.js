fb7b41d0277db080a4c6c2b1d0d101fdffa62eb1
Patricia Eihara <pateihara@gmail.com>
2023-11-05 06:38:49 -0300

    ataulizacao

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 import controller from "../controllers/orderController.js";
 
 // READ - leitura de todos os usuários
-router.get("/", controller.getOrderAll);
+router.get("/:id", controller.getOrderAll);
 
 // CREATE - criação de novos usuários
-router.post("/", controller.createOrder);
+router.post("/:id", controller.createOrder);
 
 export default router;
